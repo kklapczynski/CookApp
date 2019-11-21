fafe54523639e0cf69ec47db90d68ecd7404e0d2
kk <k.klapczynski@gmail.com>
2019-11-21 11:11:44 +0100

    Toggle open using document:click and event.target

--- src/app/shared/open.directive.ts
+++ src/app/shared/open.directive.ts
@@ -4,14 +4,19 @@ import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core'
   selector: '[appOpen]'
 })
 export class OpenDirective {
-  // constructor(private el: ElementRef) { }
-  constructor() { }
+  constructor(private el: ElementRef) { }
   
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // opens when click on host element and closes dropdown when click anywhere (outside or inside of host el.) 
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.el.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
   // @HostListener('click') onClick() {
   //   this.el.nativeElement.classList.toggle('open');
   // }
