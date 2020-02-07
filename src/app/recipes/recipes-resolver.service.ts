ac0292989e9596635fa0b016cb0fb198da6d1f84
kk <k.klapczynski@gmail.com>
2020-02-07 16:35:40 +0100

    283: Bug fix: resolver loads new data only when no data yet in app.

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,6 +3,7 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/r
 
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipesService } from './recipes.service';
 
 @Injectable({
     providedIn: 'root'
@@ -12,9 +13,17 @@ import { DataStorageService } from '../shared/data-storage.service';
 // it removes a bug when refreshing site in edit or details route
 
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService){}
+    constructor(private dataStorageService: DataStorageService, private recipesService: RecipesService){}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataStorageService.fetchRecipes();  // no subscribe here cause angular resolver does it
+        // to ensure that local changes - in app are not overwritten by data from firebase server (resolver runs and loads data each time route with resolver is accessed)
+        // we need to check if there is data and ONLY when there is none load it
+        const recipes = this.recipesService.getRecipes();
+        if (recipes.length === 0) {
+            return this.dataStorageService.fetchRecipes();  // no subscribe here cause angular resolver does it
+        } else {
+            return recipes;
+        }
+        
     }
 }
