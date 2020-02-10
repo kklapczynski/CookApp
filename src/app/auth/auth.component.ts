a2ee5bb692289d627ce6aa6cd46ed1f3294552ca
kk <k.klapczynski@gmail.com>
2020-02-10 15:33:53 +0100

    297: Refactor subscription to login and sign up and error handling.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-auth',
@@ -21,22 +22,26 @@ export class AuthComponent {
 
         this.isLoading = true;
 
-        if(this.isLoginMode) {
-            // TODO
-        } else {
+        let authObs: Observable<AuthResponseData>;
 
-            this.authService.signup(email, password).subscribe(
-                responsedata => {
-                    console.log(responsedata);
-                    this.isLoading = false;
-                },
-                errorMessage => {
-                    // console.log(errorMessage);
-                    this.error = errorMessage;
-                    this.isLoading = false;
-                }
-            )
+        if(this.isLoginMode) {
+            authObs = this.authService.login(email, password);
+        } else {
+            authObs = this.authService.signup(email, password);
+            
         }
+
+        authObs.subscribe(
+            responsedata => {
+                console.log(responsedata);
+                this.isLoading = false;
+            },
+            errorMessage => {
+                console.log(errorMessage);
+                this.error = errorMessage;
+                this.isLoading = false;
+            }
+        )
         
         form.reset();
     }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AuthComponent } from './auth.component';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
 // new interface to be used only here in this service: to set up shape of data coming back from signing up
 // it is not required, but good practice in Angular
 // properties are from firebase auth docs
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({
@@ -32,22 +32,44 @@ export class AuthService{
                 returnSecureToken: true
             }
         ).pipe( 
-            catchError( errorRes => {
-                let errorMessage = 'An unknown error occured !';
-                if(!errorRes.error || !errorRes.error.error) {
-                    return throwError(errorMessage);
-                }
-                // console.log(errorRes.error.error.message);
-                switch (errorRes.error.error.message) {
-                    case 'EMAIL_EXISTS':
-                        errorMessage = 'This email exists already.';
-                        break;
-                    case 'INVALID_EMAIL':
-                        errorMessage = 'This email address invalid.';
-                        break;
-                }
-                return throwError(errorMessage);
-            })
-        )
+            catchError(this.handleError)
+        );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAmd5KQQcyeRR0vw9I9idEk2DjUMYCy9p4',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe( 
+            catchError(this.handleError)
+        );
+    }
+
+    // private method cause we use it only in this service
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occured !';
+        if(!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        // console.log(errorRes.error.error.message);
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already.';
+                break;
+            case 'INVALID_EMAIL':
+                errorMessage = 'This email address invalid.';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email address was not found.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct.';
+                break;
+        }
+        return throwError(errorMessage);
     }
 }
